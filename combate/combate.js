391cb5a229a2912ede79ff440fcaeae4c80e461c
ThiagoToWo <towo497@gmail.com>
2022-01-10 04:02:01 -0300

    resolvido o problema do retorno do vencedor

--- combate/combate.js
+++ combate/combate.js
@@ -1,7 +1,7 @@
 /*
 *função recursiva que administra turnos de ataque e defesa entre dois lutadores.
 *Os lutadores são objetos como o modelo abaixo
-*	
+*
 *	lutador = {
 *		nome: '...',
 *		turno: 0 (atacante) ou 1 (defensor),
@@ -16,19 +16,19 @@
 *	}
 *
 *Os lutadores precisam ter seus turnos configurados com 0 (atacante) ou 1 (defensor)
-*antes de serem passados para a função. 
-*	
+*antes de serem passados para a função.
+*
 *	//looping do jogo
 *	l1.turno = 0 // atacante
 *	l2.turno = 1 // defensor
 *	lutar(l1, l2)
 *
-*Com o valor inicial dos turnos, a função executa o primeiro ataque e alterna os 
+*Com o valor inicial dos turnos, a função executa o primeiro ataque e alterna os
 *atacantes e defensores até ocorrer um vencedor.
 *Com uma vitória decidida, a função usa a auxiliar _atualizar() para atuaizar os
-*ganhos do vencedor e retorna ele para ser aplicado em um próximo combate. 
+*ganhos do vencedor e retorna ele para ser aplicado em um próximo combate.
 *
-*A função também pode usar como entrada uma string (relator) representando como 
+*A função também pode usar como entrada uma string (relator) representando como
 *os eventos da luta serão relatados.
 *	'console', undefined ou vazio: imprime os eventos no console.log().
 *	'alert': imprime os eventos por alert().
@@ -39,13 +39,13 @@
 function lutar(lutador1, lutador2, relator) {
 	// string identificadora do tipo de relator e eventos do combate
 	var log = relator;
-	
+
 	// calcula a fração de ataque, magia e defesa a serem usadas e se vai usar magia
 	var teste_foco = Math.random();
 	var teste_reflexo = Math.random();
-	var teste_habilidade = Math.random();	
+	var teste_habilidade = Math.random();
 	var teste_magia = Math.random();
-	
+
 	// decide quem vai ser o atacante e o defensor pelo valor inicial do turno
 	var atacante = null;
 	var defensor = null;
@@ -62,37 +62,37 @@ function lutar(lutador1, lutador2, relator) {
 	var ataque = 0;
 	var magia = 0;
 	var defesa = 0;
-	
+
 	// passou no teste de foco, ataca
 	if (teste_foco <= atacante.foco) {
 		ataque = atacante.ataque * teste_foco;
-		_relatar(atacante.nome, 'efetuou golpe', ataque.toFixed(), '/', atacante.ataque);
+		_relatar(atacante.nome, 'efetuou golpe', ataque.toFixed(), '/', atacante.ataque.toFixed());
 	} else {
 		_relatar(atacante.nome, 'errou o golpe');
 	}
-	
+
 	// passou no teste de magia e no teste de habilidade, usa a magia
 	if (teste_magia <= atacante.estilo && teste_habilidade <= atacante.habilidade) {
 		magia = atacante.magia * teste_habilidade;
-		_relatar(atacante.nome, 'usou magia', magia.toFixed(), '/', atacante.magia);
+		_relatar(atacante.nome, 'usou magia', magia.toFixed(), '/', atacante.magia.toFixed());
 	} else {
 		_relatar(atacante.nome, 'não usou ou errou a magia');
 	}
-	
+
 	// teve ataque com golpe ou magia e passou no teste de defesa
 	if ((ataque || magia) && teste_reflexo <= defensor.reflexo) {
 		defesa = defensor.defesa * teste_reflexo;
-		_relatar(defensor.nome, 'conseguiu defender', defesa.toFixed(), '/', defensor.defesa);
+		_relatar(defensor.nome, 'conseguiu defender', defesa.toFixed(), '/', defensor.defesa.toFixed());
 	} else {
 		_relatar(defensor.nome, 'não defendeu');
 	}
-	
+
 	// calcula o dano que o ataque causou e retira da vida do defensor
 	if (ataque + magia > defesa) {
 		var resultado = (ataque + magia) - defesa;
-		defensor.vida -= resultado;		
+		defensor.vida -= resultado;
 		_relatar(defensor.nome, 'recebeu', resultado.toFixed(),
-					'de dano e ficou com', defensor.vida.toFixed(), 'de vida.');		
+					'de dano e ficou com', defensor.vida.toFixed(), 'de vida.');
 	} else {
 		_relatar(defensor.nome, 'não teve danos');
 	}
@@ -104,9 +104,9 @@ function lutar(lutador1, lutador2, relator) {
 	} else { // senão, alterna os turnos e repete o ataque
 		lutador1.turno = (lutador1.turno + 1) % 2;
 		lutador2.turno = (lutador2.turno + 1) % 2;
-		lutar(lutador1, lutador2, log);
-	} 
-	
+		return lutar(lutador1, lutador2, log);
+	}	
+
 	// função auxiliar para atualizar os lutadores vencedores
 	function _atualizar(lutador) {
 		lutador.vida *= 1.2;
@@ -127,29 +127,29 @@ function lutar(lutador1, lutador2, relator) {
 			console.log(...entradas);
 		} else if (log === 'alert') {
 			var texto = '';
-			
+
 			for (var i = 0; i < entradas.length; i++) {
 				texto += entradas[i] + ' ';
 			}
-			
+
 			alert(texto);
 		} else if (log === 'both') {
 			var texto = '';
-			
+
 			for (var i = 0; i < entradas.length; i++) {
 				texto += entradas[i] + ' ';
 			}
-			
+
 			alert(texto);
 			console.log(...entradas);
 		} else {
 			new Error("Valor inválido de relator de eventos. Escolha 'console', 'alert' ou 'both'.");
 		}
-	}	
+	}
 }
 
 /*
-*Função que cria um objeto lutador dado como entrada um nome. 
+*Função que cria um objeto lutador dado como entrada um nome.
 */
 function criarLutador(nome) {
 	var lutador = {
@@ -164,18 +164,18 @@ function criarLutador(nome) {
 		reflexo: _random(0.3, 1),
 		habilidade: _random(0.3, 1)
 	};
-	
+
 	return lutador;
 }
 
 /*
 *Função que cria um array de objetos lutadores dado como entrada um array de
-*nomes. 
+*nomes.
 */
 function criarLutadores(...nomes) {
 	var lutadores = [];
 	var lutador;
-	
+
 	for (var i = 0; i < nomes.length; i++) {
 		lutador = {
 			nome: nomes[i],
@@ -189,7 +189,7 @@ function criarLutadores(...nomes) {
 			reflexo: _random(0.3, 1),
 			habilidade: _random(0.3, 1)
 		}
-		
+
 		lutadores.push(lutador);
 	}
 
@@ -199,7 +199,7 @@ function criarLutadores(...nomes) {
 /*
 *Função usada para criar um div posicionado no corpo do documento e
 *representar nele as informações do objeto lutador passado como parâmetro.
-*As entradas left e top, são usadas para a posição CSS div.style.left e 
+*As entradas left e top, são usadas para a posição CSS div.style.left e
 *div.style.top.
 */
 function representarLutador(lutador, left, top) {
@@ -211,28 +211,28 @@ function representarLutador(lutador, left, top) {
 	div.style.border = 'solid';
 	div.style.padding = '10px';
 	div.style.background = '#efefef';
-	
+
 	for (var propriedade in lutador) {
 		if (propriedade === 'nome' || propriedade === 'turno') {
-			div.innerHTML += '<b>- ' + propriedade + '</b>: ' 
+			div.innerHTML += '<b>- ' + propriedade + '</b>: '
 							 + lutador[propriedade] + '<br/>';
 		} else if (propriedade === 'vida' || propriedade === 'ataque' ||
 				   propriedade === 'defesa' || propriedade === 'magia'){
-			div.innerHTML += '<b>- ' + propriedade + '</b>: ' 
+			div.innerHTML += '<b>- ' + propriedade + '</b>: '
 							 + Number(lutador[propriedade]).toFixed() + '<br/>';
 		} else {
-			div.innerHTML += '<b>- ' + propriedade + '</b>: ' 
+			div.innerHTML += '<b>- ' + propriedade + '</b>: '
 							 + Number(lutador[propriedade]).toFixed(2) + '<br/>';
 		}
 	}
-	
+
 	document.body.appendChild(div);
 }
 
 /*
 *Função usada para criar um div posicionado no corpo do documento e
 *representar nele as informações dos objetos lutadores passados como parâmetros.
-*As entradas left e top, são usadas para a posição CSS div.style.left e 
+*As entradas left e top, são usadas para a posição CSS div.style.left e
 *div.style.top do primeiro lutador. A posição do segundo é calculada ao lado.
 */
 function representarLutadores(lutador1, lutador2, left, top) {
@@ -246,12 +246,22 @@ function representarLutadores(lutador1, lutador2, left, top) {
 */
 function _random(inicio, fim) {
 	var rnd;
-	
+
 	if (inicio >= 1 && fim >= 1) {
 		rnd = Math.floor(inicio + Math.random() * (fim - inicio + 1));
 	} else {
 		rnd = (inicio + Math.random() * (fim - inicio));
 	}
-	
+
 	return rnd;
+}
+
+function _copiarObjeto(o) {
+	var copia = {};
+	
+	for (var p in o) {
+		copia.p = o[p];
+	}
+	
+	return copia;
 }
